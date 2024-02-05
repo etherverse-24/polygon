7ecabd36fc2da31baa6b783126fa3eef3cbedadf
Raman Agrawal <iramanagrawal@gmail.com>
2024-02-05 22:06:40 +0530

    +game section

--- src/ui/game/Game.tsx
+++ src/ui/game/Game.tsx
@@ -1,13 +1,14 @@
+import GameCard from "../../components/GameCard";
 import { games } from "../../constents";
 
 const Game = () => {
   return (
-    <section className="px-2 lg:px-32 py-20 min-h-lvh">
+    <section id="games" className="px-2 lg:px-32 py-20 min-h-lvh">
       <h1 className="text-white text-5xl text-center">Games</h1>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pt-10 ">
         {
             games.map((game, index) => (
-                <div key={index} className="w-full mx-auto h-96 bg-[#171717]"></div>
+               <GameCard item={game} key={index}/>
             ))
         }
        
