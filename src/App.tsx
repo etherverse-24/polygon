c461f1285488a2541f8ffe5aab44c7c5cd9673c4
Raman Agrawal <iramanagrawal@gmail.com>
2024-02-05 21:05:50 +0530

    game section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Featured from './ui/featured/Featured'
 import Footer from './ui/footer/Footer'
+import Game from './ui/game/Game'
 import Games from './ui/games/Games'
 import Hero from './ui/hero/Hero'
 import Navbar from './ui/navbar/Nav'
@@ -12,6 +13,7 @@ function App() {
  <Navbar/>
  <Hero/>
  <Featured/>
+ <Game/>
  <Games/>
  <Footer/>
  </>
