54c36644e393103983b4c216fbb0d24191c4b9c1
tanyamehra085 <134936400+tanyamehra085@users.noreply.github.com>
2024-02-05 17:28:32 +0530

    +featured

--- src/ui/featured/Featured.tsx
+++ src/ui/featured/Featured.tsx
@@ -2,7 +2,10 @@
 
 const Featured = () => {
   return (
-    <section id="featured">Featured</section>
+    <section id="featured" className="min-h-[50vh] p-14  text-white relative">Featured Game
+    
+    <video className="absolute border-2 top-10 right-40 h-[400px]" src="https://player.vimeo.com/progressive_redirect/playback/808950926/rendition/720p/file.mp4?loc=external&signature=d2e4ab64d75abc4c9c1d1adcfb5a86b7afa250d5844883de6fef34026b69a0f2" muted autoPlay></video>
+    </section>
   )
 }
 
