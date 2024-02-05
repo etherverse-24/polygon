b12fb89d89aaddc68a0ead2845e508c66505d201
Raman Agrawal <iramanagrawal@gmail.com>
2024-02-05 18:57:01 +0530

    feature

--- src/ui/featured/Featured.tsx
+++ src/ui/featured/Featured.tsx
@@ -3,9 +3,10 @@ import "./feature.scss";
 const Featured = () => {
   return (
     <section id="featured" className="min-h-[70vh]  text-white relative">
+      <div className="test"></div>
       {/* <img src="/bg.png" className="absolute w-full h-full opacity-50 -z-10" alt="" /> */}
-      <div className="relative   w-[85%] mx-auto">
-        <div className="pt-32 w-1/2 relative z-2">
+      <div className="relative z-10 lg:w-[85%] mx-auto">
+        <div className="pt-32 lg:w-1/2 ">
           <h1 className="text-3xl">Our Featured Game</h1>
           <h1 className="text-7xl pt-10">MetaDogeUnity</h1>
           <h2 className="text-lg pt-10">
@@ -13,10 +14,6 @@ const Featured = () => {
             ACCOUNT ABSTRACTION AND BITCOIN
           </h2>{" "}
         </div>
-        {/* <div className='uuu1'>
-    <video className="absolute uuu top-10 right-40 h-[400px]" src="https://player.vimeo.com/progressive_redirect/playback/808950926/rendition/720p/file.mp4?loc=external&signature=d2e4ab64d75abc4c9c1d1adcfb5a86b7afa250d5844883de6fef34026b69a0f2" muted autoPlay></video>
-
-    </div> */}
         <div className="hero-video-product-page is-game ">
           <div className="overlay" />
           <div className="games-main-img-inner">
