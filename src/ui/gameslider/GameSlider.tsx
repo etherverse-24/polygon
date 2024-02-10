5a857feaa11e511db796feac1afa237d3899a35b
tanyamehra085 <134936400+tanyamehra085@users.noreply.github.com>
2024-02-10 14:07:43 +0530

    +add games video

--- src/ui/gameslider/GameSlider.tsx
+++ src/ui/gameslider/GameSlider.tsx
@@ -1,10 +1,11 @@
 import "./gameslider.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Navigation, Pagination, Autoplay ,EffectFade} from "swiper/modules";
 
 // Import Swiper styles
 import "swiper/css";
+import 'swiper/css/effect-fade';
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
@@ -13,43 +14,43 @@ const GameSlider = () => {
     video: string;
     heading: string;
     desc: string;
+    img:string;
   }
   const slides: Slides[] = [
     {
-      video: "./bg1-video.mp4",
-      heading: "  MetaDogeUnity",
-      desc: ` MULTICHAIN METAVERSE AAA GAMEFI PLATFORM THAT COMBINES THE POWER OF ACCOUNT ABSTRACTION AND BITCOIN`,
+      video: "./1.mp4",
+      heading: "The Mask World Adventure Game",
+      desc: `Fight the mask, caveman, sharks, spiders, spiky turtles and many other monsters. With the right strategy you will defeat all your enemies in the game!`,
+      img:"https://play-lh.googleusercontent.com/JbOyeoXDjtSz-aiCIi53sUfC3q0NWZEPdCJwtsnXUC5UJS-bXhf5n9wbxJ9Mvby5Ag=w240-h480-rw"
     },
     {
       video: "./bg1-video.mp4",
       heading: "  MetaDogeUnity",
       desc: ` MULTICHAIN METAVERSE AAA GAMEFI PLATFORM THAT COMBINES THE POWER OF ACCOUNT ABSTRACTION AND BITCOIN`,
+      img:''
     },
     {
-      video: "./V2-1.mp4",
-      heading: "Adrak",
-      desc: ` MULTICHAIN METAVERSE AAA GAMEFI PLATFORM THAT COMBINES THE POWER OF ACCOUNT ABSTRACTION AND BITCOIN`,
+      video: "./4.mp4",
+      heading: "Stickman Warrior Fighting Wars",
+      desc: `Stickman Shadow Warrior is great-to-play of the new shadow stickman style games in a fantasy dark world theme. A great combination of action, role playing games.`,
+      img:'game3.png'
     },
   ];
   return (
     <section id="gameslider" className="min-h-[80vh] z-10 text-white relative">
       <Swiper
-        // install Swiper modules
-        modules={[Navigation, Pagination, Autoplay]}
-        //   spaceBetween={50}
-        slidesPerView={1}
-        effect="fade"
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
-        navigation={{
+         spaceBetween={30}
+         effect={'fade'}
+         navigation={{
           nextEl: null, // Hide next arrow
           prevEl: null, // Hide previous arrow
         }}
-        
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
+         pagination={{
+           clickable: true,
+         }}
+         scrollbar={{ draggable: true }}
+         autoplay ={{delay:5000}}
+         modules={[EffectFade, Navigation, Pagination,Autoplay]}
         className="relative -z-0 min-h-[80vh] h-full"
       >
         {slides.map((item, i) => (
@@ -65,8 +66,8 @@ const GameSlider = () => {
             ></video>
             <div className="relative z-10 lg:w-[85%] mx-auto">
               <div className="pt-10 lg:pt-32 lg:w-1/2 ">
-                <h1 className="text-4xl text-center lg:text-left">Our Game</h1>
-                <h1 className="text-4xl lg:text-7xl pt-10 text-center lg:text-left">
+                <h1 className="text-4xl text-center lg:text-left">Our Games</h1>
+                <h1 className="text-4xl lg:text-4xl pt-10 text-center lg:text-left">
                   {item.heading}
                 </h1>
                 <h2 className="text-lg pt-10 text-center lg:text-left">
@@ -77,12 +78,12 @@ const GameSlider = () => {
                 <div className="overlay" />
                 <div className="games-main-img-inner">
                   <div className="games-main-img-content">
-                    {/* <img
-                      src="https://www.metadogeunity.in/hero.png"
+                    <img
+                      src={item.img}
                       loading="lazy"
                       alt=""
                       className="games-main-img-logo"
-                    /> */}
+                    />
                     <a
                       href="/play/guild-of-guardians"
                       className="bg-[rgba(247,247,247,.1)] p-4 rounded-3xl cursor-pointer"
