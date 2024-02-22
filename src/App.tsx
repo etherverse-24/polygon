b4e1e024277980ba25eb2949ae3f9ca6b2d54a51
Raman Agrawal <iramanagrawal@gmail.com>
2024-02-22 23:02:43 +0530

    game

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import './App.css'
-import Featured from './ui/featured/Featured'
-import Footer from './ui/footer/Footer'
-import Game from './ui/game/Game'
-import Latestgames from './ui/latestgames/Latestgames'
-import Hero from './ui/hero/Hero'
-import Navbar from './ui/navbar/Nav'
-import GameSlider from './ui/gameslider/GameSlider'
-import Slider from './ui/slider/Slider'
-import { useEffect } from 'react'
-import Aos from 'aos';  
+import "./App.css";
+import Featured from "./ui/featured/Featured";
+import Footer from "./ui/footer/Footer";
+import Game from "./ui/game/Game";
+import Latestgames from "./ui/latestgames/Latestgames";
+import Hero from "./ui/hero/Hero";
+import Navbar from "./ui/navbar/Nav";
+import GameSlider from "./ui/gameslider/GameSlider";
+import { useEffect } from "react";
+import Aos from "aos";
 import "aos/dist/aos.css";
-import Slider2 from './ui/slider2/slider2'
+import Slider2 from "./ui/slider2/slider2";
 function App() {
-useEffect(() => {
-  Aos.init({duration: 1000});
-}
-, [])
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
 
   return (
- <>
- <Navbar/>
- <Hero/>
- <Featured/>
- <Latestgames/>
- <Slider2/>
- <GameSlider/>
- <Game/>
- <Slider/>
- <Footer/>
- </>
-  )
+    <>
+      <Navbar />
+      <Hero />
+      <Featured />
+      <Latestgames />
+      <Slider2
+        video="./game3.mp4"
+        heading="The Jewel Hunt"
+        desc="Fight the mask, caveman, sharks, spiders, spiky turtles and many other monsters. With the right strategy you will defeat all your enemies in the game!"
+      />
+      <GameSlider />
+      <Game />
+      <Slider2
+        video="./game2.mp4"
+        heading="The Jewel Hunt"
+        desc="Fight the mask, caveman, sharks, spiders, spiky turtles and many other monsters. With the right strategy you will defeat all your enemies in the game!"
+      />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/ui/slider2/slider2.tsx
+++ src/ui/slider2/slider2.tsx
@@ -1,49 +1,32 @@
+import { FC } from "react";
 import Button from "../../components/Button";
-const Slider2 = () => {
-  interface Slides {
-    video: string;
-    heading: string;
-    desc: string;
-    img: string;
-  }
-  const slides: Slides[] = [
-    {
-      video: "./game3.mp4",
-      heading: "The Jewel Hunt",
-      desc: `Fight the mask, caveman, sharks, spiders, spiky turtles and many other monsters. With the right strategy you will defeat all your enemies in the game!`,
-      img: "https://play-lh.googleusercontent.com/JbOyeoXDjtSz-aiCIi53sUfC3q0NWZEPdCJwtsnXUC5UJS-bXhf5n9wbxJ9Mvby5Ag=w240-h480-rw",
-    },
-  ];
+interface Slides {
+  video: string;
+  heading: string;
+  desc: string;
+  img?: string;
+}
+const Slider2: FC<Slides> = ({ heading, desc, video }) => {
   return (
     <section id="" className="min-h-[80vh] z-10 text-white relative bg-black">
-      
-        {slides.map((item, i) => (
-          <div
-            key={i}
-            className=" h-full min-h-[80vh] lg:w-[85%] mx-auto flex flex-wrap "
-          >
-             <div className="lg:w-1/2 lg:pt-32 ">
-            <div className="text-4xl mb-10 font-bold">{item.heading}</div>
-            <div className="text-lg">{item.desc}</div>
-            <div className="pt-10 w-1/3">
-          <Button text={"View Game"} />
-
-            </div>
-            </div>
-            <div className="lg:w-1/2 max-h-[550px] flexCenter  ">
-              <video
-                src={item.video}
-                autoPlay
-                muted
-                loop
-                className="h-[550px] w-[320px] object-fill "
-              ></video>
-            </div>{" "}
-          
-         
+      <div className=" h-full min-h-[80vh] lg:w-[85%] p-10 lg:p-0 mx-auto flex flex-wrap ">
+        <div className="lg:w-1/2 lg:pt-32 text-center lg:text-left">
+          <div className="text-4xl mb-10 font-bold">{heading}</div>
+          <div className="text-lg">{desc}</div>
+          <div className="pt-10 lg:w-1/3 w-2/3 mx-auto lg:mr-auto lg:ml-0 ">
+            <Button text={"View Game"} />
           </div>
-        ))}
-      
+        </div>
+        <div className="lg:w-1/2 max-h-[550px] flexCenter  ">
+          <video
+            src={video}
+            autoPlay
+            muted
+            loop
+            className="h-[550px] w-[320px] object-fill "
+          ></video>
+        </div>{" "}
+      </div>
     </section>
   );
 };
