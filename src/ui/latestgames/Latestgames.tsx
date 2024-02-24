3a6c4608650de7e1bf122656eb371c8180dc1e98
Raman Agrawal <iramanagrawal@gmail.com>
2024-02-24 13:24:16 +0530

    latest

--- src/ui/latestgames/Latestgames.tsx
+++ src/ui/latestgames/Latestgames.tsx
@@ -7,10 +7,12 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import GameCard from "../../components/GameCard";
+import Heading from "../../components/heading/Heading";
 const Latestgames = () => {
   return (
     <section id="games" className="px-2 lg:px-28 py-20 min-h-lvh bg-[#0e0e0e]">
-      <h1 className="text-white text-5xl font-semibold text-center uppercase">Latest Games</h1>
+      <Heading text="Latest Games"/>
+      {/* <h1 className="text-white t ext-5xl font-semibold text-center uppercase"></h1> */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-3 pt-10 ">
         {latest_games.map((item, i) => (
           <GameCard item={item} key={i} />
