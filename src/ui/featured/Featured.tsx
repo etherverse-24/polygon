008fee32430d194f4c73376dc6d7aa72c22b2a4e
tanyamehra085 <134936400+tanyamehra085@users.noreply.github.com>
2024-02-24 16:39:48 +0530

    hh

--- src/ui/featured/Featured.tsx
+++ src/ui/featured/Featured.tsx
@@ -5,7 +5,7 @@ const Featured = () => {
   return (
     <section
       id="featured"
-      className="min-h-[80vh] z-10 text-white relative "
+      className="lg:min-h-[80vh]  z-10 text-white relative "
     >
       <div className="test"></div>
       {/* <img src="/bg.png" className="absolute w-full h-full opacity-50 -z-10" alt="" /> */}
