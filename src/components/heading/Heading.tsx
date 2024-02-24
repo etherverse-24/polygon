a68be591cc786a89a57c772de15b9873d84e1220
Raman Agrawal <iramanagrawal@gmail.com>
2024-02-24 13:55:16 +0530

    heading

--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -3,14 +3,16 @@ import { FC } from "react";
 interface headingProps {
   text: string;
   className?: string;
-  shadow?:boolean
+  shadow?: boolean;
 }
 
-const Heading: FC<headingProps> = ({ text, className ,shadow}) => {
+const Heading: FC<headingProps> = ({ text, className, shadow }) => {
   return (
     <h1
-      className={`main-heading text-white text-5xl font-semibold  uppercase ${className} `}
-      style={{  textShadow:`${shadow && " 0 0 46px white"}`}}
+      className={`main-heading ${
+        shadow && "text-white"
+      } text-5xl font-semibold uppercase ${className} `}
+      style={{ textShadow: `${shadow && " 0 0 46px white"}` }}
     >
       {text}
     </h1>
