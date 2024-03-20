bb3e34f9d6a80af09bfe43f1d45dac21edb981d4
Raman Agrawal <iramanagrawal@gmail.com>
2024-03-21 01:20:56 +0530

    fix build

--- src/ui/slider2/slider2.tsx
+++ src/ui/slider2/slider2.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import Button from "../../components/Button";
+// import Button from "../../components/Button";
 import Heading from "../../components/heading/Heading";
 interface Slides {
   video: string;
