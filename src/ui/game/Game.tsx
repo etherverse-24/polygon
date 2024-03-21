4f03b4c00fd6f1658790e727d7afe63837056f0d
Raman Agrawal <iramanagrawal@gmail.com>
2024-03-21 13:33:09 +0530

    category tab in game

--- src/ui/game/Game.tsx
+++ src/ui/game/Game.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import GameCard from "../../components/GameCard";
 import Heading from "../../components/heading/Heading";
-import { games } from "../../constents";
+import { games, GameType } from "../../constents"; // Assuming games and GameType are properly defined
+import Tabs from "./Tabs";
 
 const Game = () => {
+  const [selectedTab, setSelectedTab] = useState<string>("All");
+
+  // Filter games based on the selected tab
+  const filteredGames: GameType[] = selectedTab === "All" ? games.All : games[selectedTab as keyof typeof games];
+
   return (
-    <section id="games_section" className="px-2 lg:px-32 py-20 min-h-lvh bg-[#0e0e0e]">
+    <section
+      id="games_section"
+      className="px-2 lg:px-32 py-20 min-h-lvh bg-[#0e0e0e]"
+    >
       {/* <img src="./diamond.webp" alt="" /> */}
       <Heading shadow text="All Games" className="text-center" />
+      <Tabs selectedTab={setSelectedTab} className="mt-5" />
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-3 pt-10 ">
-        {games.map((game, index) => (
-          <GameCard item={game} key={index} comingSoon={game.coming_soon} i={index}/>
+        {filteredGames.map((game: GameType, index: number) => (
+          <GameCard
+            item={game}
+            key={index}
+            comingSoon={game.coming_soon}
+            i={index}
+          />
         ))}
       </div>
     </section>
---
+++ src/ui/game/Tabs.tsx
@@ -0,0 +1,53 @@
+import { FC, useState } from "react";
+
+interface TabsProps extends React.HTMLAttributes<HTMLDivElement> {
+  selectedTab: (tab: string) => void;
+  className?: string;
+}
+
+const Tabs: FC<TabsProps> = ({ selectedTab, className, ...props }) => {
+  const [activeTab, setActiveTab] = useState<string>("All"); // [1, 2, 3, 4
+  const handleTabChange = (tab: string) => {
+    setActiveTab(tab);
+    selectedTab(tab);
+  };
+  const tabs = [
+    {
+      name: "All",
+      id: 1,
+    },
+    {
+      name: "New",
+      id: 2,
+    },
+    {
+      name: "Popular",
+      id: 3,
+    },
+    {
+      name: "Slots",
+      id: 4,
+    },
+  ];
+
+  return (
+    <div
+      {...props}
+      className={` ${className} w-full h-12  bg-[#202329] max-w-xl mx-auto rounded-lg  grid p-1  grid-cols-4 gap-2`}
+    >
+      {tabs.map((tab) => (
+        <button
+          key={tab.id}
+          className={` w-full h-full ${
+            activeTab === tab.name && "bg-[#343843]"
+          } text-white flexCenter rounded-lg hover:bg-purple-500 active:bg-purple-700 transition-all duration-300`}
+          onClick={() => handleTabChange(tab.name)}
+        >
+          {tab.name}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Tabs;
